52618331758633b7ff2d5b188b7ef21e59adcd58
Yousef Ali Omar Quran <yquran@mum.edu>
2019-09-05 18:08:41 -0500

    Initial Commit ...

---
+++ GitTest/src/edu/mum/cs/git/first/GitTest.java
@@ -0,0 +1,10 @@
+package edu.mum.cs.git.first;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git is fun");
+	}
+
+}
