dca98fb54247e1ada8d405ade4d3d9ed55d09da1
Yousef Ali Omar Quran <yquran@mum.edu>
2019-09-05 18:10:09 -0500

    adjusts example output message

--- GitTest/src/edu/mum/cs/git/first/GitTest.java
+++ GitTest/src/edu/mum/cs/git/first/GitTest.java
@@ -4,7 +4,7 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Git is fun");
+		System.out.println("Git is cool!");
 	}
 
 }
