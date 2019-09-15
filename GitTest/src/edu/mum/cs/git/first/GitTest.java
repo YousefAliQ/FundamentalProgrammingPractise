2304a70c51f5f78c7b8bafcd73b480cea25df626
Yousef Ali Omar Quran <yquran@mum.edu>
2019-09-15 09:21:51 -0500

    just for testing ...

--- GitTest/src/edu/mum/cs/git/first/GitTest.java
+++ GitTest/src/edu/mum/cs/git/first/GitTest.java
@@ -5,6 +5,11 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git is cool!");
+		int i=0;
+		for (i++, System.out.println("hello");;System.out.println("hello"), i++ ) {
+			if (i == 10)
+				break;
+		}
 	}
 
 }
