92dc010505fce8c736f7b040919948550bfae3a0
Yousef Ali Omar Quran <yquran@mum.edu>
2019-09-28 14:34:49 -0500

    Week2 -> Day4 -> Lab.

---
+++ W2D4Lab/src/HelloWorld.java
@@ -0,0 +1,18 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World3");
+
+		int x = 5;
+		x++;
+		assert x == 2 : "x is " + x; // Exception in thread "main" java.lang.AssertionError: x is 6
+
+		System.out.println("Hello World4");
+		System.out.println("Hello World5");
+
+	}
+
+}
