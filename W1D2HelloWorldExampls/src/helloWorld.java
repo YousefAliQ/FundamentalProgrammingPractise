b8d8473b2a0742af581f69075016d9b87b14b581
Yousef Ali Omar Quran <yquran@mum.edu>
2019-09-28 14:24:27 -0500

    Hello world example

---
+++ W1D2HelloWorldExampls/src/helloWorld.java
@@ -0,0 +1,15 @@
+
+import java.io.*;
+
+import javax.swing.JOptionPane;
+
+class helloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+		 String x = JOptionPane.showInputDialog("Enter the value");
+		 System.out.println(x);
+
+	} // end main() String
+} // end class
